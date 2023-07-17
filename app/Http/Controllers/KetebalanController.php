7d39d1175dbaf93a8c973f43a30113fa23d8433e
wensen2525 <109505910+wensen2525@users.noreply.github.com>
2023-07-17 22:30:50 +0700

    123

---
+++ app/Http/Controllers/KetebalanController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ketebalan;
+use App\Http\Requests\StoreKetebalanRequest;
+use App\Http\Requests\UpdateKetebalanRequest;
+
+class KetebalanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKetebalanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ketebalan $ketebalan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Ketebalan $ketebalan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKetebalanRequest $request, Ketebalan $ketebalan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ketebalan $ketebalan)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/WarnaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Warna;
+use App\Http\Requests\StoreWarnaRequest;
+use App\Http\Requests\UpdateWarnaRequest;
+
+class WarnaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreWarnaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Warna $warna)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Warna $warna)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateWarnaRequest $request, Warna $warna)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Warna $warna)
+    {
+        //
+    }
+}
