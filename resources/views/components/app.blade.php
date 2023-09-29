9d41397d1196feff28d4b48749df5427cee3013a
stefanusjonatan <stefanus.jonatan@binus.ac.id>
2023-09-29 15:10:07 +0700

    update favicon

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -8,6 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" type="image/x-icon" href="{{ url('./storage/gambar/logo-color.ico') }}">
     <title>{{ config('app.name', 'CV TRI JAYA') }}</title>
     <!-- Scripts -->
     @vite(['resources/js/app.js'])
