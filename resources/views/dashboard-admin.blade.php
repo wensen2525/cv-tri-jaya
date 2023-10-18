142f20164288cef5553c4ed63d99d16e6566e117
wensen2525 <109505910+wensen2525@users.noreply.github.com>
2023-10-18 14:10:09 +0700

    update

--- resources/views/dashboard-admin.blade.php
+++ resources/views/dashboard-admin.blade.php
@@ -1,7 +1,7 @@
 <x-app>
       <div class="container p-5">
             <div class="row">
-                  <div class="col-7">
+                  <div class="col-12">
                         <div class="row">
                               <div class="col-12">
                                     <b class="fs-4" style="color: #4C48FF">Hello Admin,</b>
@@ -11,16 +11,16 @@
                                     <p class="fw-semibold" style="font-size: 20px;color: #4C48FF">Total</p>
                               </div>
                               <div class="col-12 pb-3">
-                                    <div class="d-flex gap-3">
-                                          <div class="col-3 p-3 bg-warning rounded-3">
+                                    <div class="gap-3" style="display:grid;grid-gap: 10px;grid-template-columns: auto auto auto;">
+                                          <div class="p-3 bg-warning rounded-3">
                                                 <p>Kaca</p>
                                                 <p class="m-0 d-flex justify-content-end fs-3 fw-bold">13</p>
                                           </div>
-                                          <div class="col-3 p-3 bg-warning rounded-3">
+                                          <div class="p-3 bg-warning rounded-3">
                                                 <p>Jenis</p>
                                                 <p class="m-0 d-flex justify-content-end fs-3 fw-bold">13</p>
                                           </div>
-                                          <div class="col-3 p-3 bg-warning rounded-3">
+                                          <div class="p-3 bg-warning rounded-3">
                                                 <p>Projek</p>
                                                 <p class="m-0 d-flex justify-content-end fs-3 fw-bold">13</p>
                                           </div>
@@ -31,13 +31,6 @@
                               </div>
                               <div class="col-12 pb-3">
 
-                              </div>
-                        </div>
-                  </div>
-                  <div class="col-5">
-                        <div class="row">
-                              <div class="col-12">
-                                    Samping
                               </div>
                         </div>
                   </div>
